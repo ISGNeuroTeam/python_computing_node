08485851c60f7dcdbe9435c7c747ca192eef293e
Artem Zenkov <azenkov@isgneuro.com>
2022-02-16 23:43:45 +0700

    test execution environment

--- python_computing_node/execution_environment/test_execution_environment/command_executor.py
+++ python_computing_node/execution_environment/test_execution_environment/command_executor.py
@@ -1,10 +1,37 @@
+from functools import partial
+
+from .test_commands import syntax, do_error_command, do_normal_command, do_subsearch_command
+
+
 class CommandExecutor:
     def __init__(self, storages: dict, commands_dir, progress_message):
         self.progress_message = progress_message
-        pass
+        self.storages = storages
+        self.commands_dir = commands_dir
+        self.depth=0
 
-    def get_commands_syntax(self):
-        return {'test_syntax': 'syntax'}
+    @staticmethod
+    def get_commands_syntax():
+        return syntax
 
-    def execute_commands(self, dict_commands):
-        return []
+    def execute(self, dict_commands):
+        for command_index, command in enumerate(dict_commands):
+            command_progress_message = partial(
+                self.progress_message,
+                command_name=command['name'],
+                command_index_in_pipeline=command_index+1,
+                total_commands_in_pipeline=len(dict_commands),
+                depth=self.depth
+            )
+            if command['name'] == 'normal_command':
+                do_normal_command(command, command_progress_message)
+            elif command['name'] == 'error_command':
+                do_error_command(command, command_progress_message)
+            elif command['name'] == 'subsearch_command':
+                self.depth += 1
+                do_subsearch_command(command, command_progress_message, self.execute)
+                self.depth -= 1
+            else:
+                raise Exception('Unsupported command')
+
+        return {}
