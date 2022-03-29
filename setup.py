1c1143f6ce0fccd67717e7e68b2c4e2b79225dfd
Artem Zenkov <azenkov@isgneuro.com>
2022-03-29 12:02:03 +0700

    change version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 __author__ = "Artem Zenkov"
 __copyright__ = "Copyright 2022, ISGNeuro"
 __credits__ = []
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __maintainer__ = "Artem Zenkov"
 __email__ = "azenkov@isgneuro.com"
 __status__ = "Develop"
