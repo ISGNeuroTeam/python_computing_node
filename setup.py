aa7ec8958165006e8b36358ce43f5ea2fb79a6c6
Artem Zenkov <azenkov@isgneuro.com>
2022-11-23 02:00:25 +0700

    add 1.1.0 section in CHANGELOG.md

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 __author__ = "Artem Zenkov"
 __copyright__ = "Copyright 2022, ISGNeuro"
 __credits__ = []
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __maintainer__ = "Artem Zenkov"
 __email__ = "azenkov@isgneuro.com"
 __status__ = "Production"
