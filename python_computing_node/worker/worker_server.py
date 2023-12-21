b5e852852b3aac24c55bf42ab34bb0eebaddfc40
Artem Zenkov <azenkov@isgneuro.com>
2023-12-21 14:32:30 +0700

    set  one gunicorn worker for python computing node worker

--- python_computing_node/worker/worker_server.py
+++ python_computing_node/worker/worker_server.py
@@ -76,6 +76,7 @@ class WorkerServer:
 
         config['timeout'] = 0
         config['loglevel'] = 'info'
+        config['workers'] = 1
         config['pidfile'] = str(Path(run_dir) / f'worker{port}.pid')
 
         handler = app
